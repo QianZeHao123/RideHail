34090b6cf57cb44e2d652470e3b4f180fedcb7d4
Zehao Qian <zehao.qian.cn@gmail.com>
2025-07-23 06:42:59 +0000

    add driver_record to the module

--- models/__init__.py
+++ models/__init__.py
@@ -10,6 +10,7 @@ from .order import Order
 from .driver import Driver
 from .weather import WeatherService
 from .driver_manager import DriverManager
+from .driver_record import DriverRecord
 
 # Controls what gets imported with `from models import *`
-__all__ = [Order, Driver, WeatherService, DriverManager]
+__all__ = [Order, Driver, WeatherService, DriverManager, DriverRecord]
